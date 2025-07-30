e611d121b994e0d9621bea3bb5f159abcb3c1df2
GuiRuchell <ruchellguilherme100@gmail.com>
2025-07-30 16:32:24 -0300

    feat: adding auth feature for create account user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import RegisterPage from '../features/student/components/RegisterForm';
+import RegisterStudentForm from '../features/student/components/RegisterStudentForm.tsx';
 import LoginPage from '../features/auth/components/LoginForm';
 import Dashboard from "../features/dashboard/Dashboard";
+import RegisterUserForm from "../features/auth/components/RegisterUserForm.tsx";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+  {/* Autenticação */}
+  <Route path="/login" element={<LoginPage />} />
+  <Route path="/auth" element={<RegisterUserForm />} />
+
+  {/* Alunos */}
+  <Route path="/register" element={<RegisterStudentForm />} />
+
+  {/* Dashboard */}
+  <Route path="/dashboard" element={<Dashboard />} />
+</Routes>
     </Router>
   );
 };
---
+++ src/services/authService.ts
@@ -0,0 +1,33 @@
+// src/features/auth/services/authService.ts
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:8080/api',
+});
+
+export interface UserRequest {
+  name: string;
+  email: string;
+  password: string;
+  registration: string;
+}
+
+export const registerUser = async (user: UserRequest) => {
+  try {
+    const response = await API.post('/users/create', user);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao registrar usuário:', error);
+    throw error;
+  }
+};
+
+export const getUserByRegistration = async (registration: string) => {
+  try {
+    const response = await API.get(`/users?registration=${registration}`);
+    return response.data;
+  } catch (error) {
+    console.error('Erro ao buscar usuário por matrícula:', error);
+    throw error;
+  }
+};
