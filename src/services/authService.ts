c6db1eeb6ee7dfa332b5ae029f66c52098611d2e
GuiRuchell <ruchellguilherme100@gmail.com>
2025-08-22 18:25:56 -0300

    feat:adding new routes for integrations

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://student-portal-core-api.onrender.com/api'
 });
 
 export interface UserRequest {
